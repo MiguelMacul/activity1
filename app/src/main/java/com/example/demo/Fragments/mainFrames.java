95d8b88a8d57892a3c1fe7190d534cea87d0379d
Miguel Alejandro Macuil Angeles <46255404+MiguelMacul@users.noreply.github.com>
2020-11-05 02:51:21 -0600

    subir

---
+++ app/src/main/java/com/example/demo/Fragments/mainFrames.java
@@ -0,0 +1,12 @@
+package com.example.demo.Fragments;
+
+
+import android.support.v4.app.Fragment;
+
+import com.example.demo.R;
+
+public class mainFrames extends Fragment {
+
+
+
+}
---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout= findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navegationview);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+    }
+
+
+}
